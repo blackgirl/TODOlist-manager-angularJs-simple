f303d410b7628ea997c7d21bc17f247a48b6b020
blackgirl <a.chornaya@gmail.com>
2014-04-01 20:00:38 +0300

    remove

--- js/app.js
+++
@@ -1,6 +0,0 @@
-var todoApp = angular.module('todoApp', []);
-
-//todoApp.addValue('rootUrlProject','/index.php/');
-
-todoApp.controller('projectController',ProjectController);
-todoApp.controller('taskController',TaskController);
